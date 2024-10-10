44c13c6b30d65ac035c24fd1d74cdf04eece3a1c
Gustavo12FH <93550411+Gustavo12FH@users.noreply.github.com>
2024-10-10 11:27:17 -0300

    Add files via upload

---
+++ Gerenciador_Mensalidades/gerenciador_mensalidades/src/main/java/com/example/gerenciador_mensalidades/GerenciadorMensalidadesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciador_mensalidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorMensalidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorMensalidadesApplication.class, args);
+	}
+
+}
