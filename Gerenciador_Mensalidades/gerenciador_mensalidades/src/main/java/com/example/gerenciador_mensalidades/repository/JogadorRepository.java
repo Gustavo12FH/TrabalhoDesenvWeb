eed523fff3c333c33d8814e11ce517cfbaaa340b
Gustavo12FH <93550411+Gustavo12FH@users.noreply.github.com>
2024-10-10 11:29:59 -0300

    Delete Gerenciador_Mensalidades/gerenciador_mensalidades directory

--- Gerenciador_Mensalidades/gerenciador_mensalidades/src/main/java/com/example/gerenciador_mensalidades/repository/JogadorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.gerenciador_mensalidades.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.gerenciador_mensalidades.model.Jogador;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Integer> {
-}
